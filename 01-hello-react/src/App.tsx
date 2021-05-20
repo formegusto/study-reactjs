3a19a4c2bf83188eb791debef8d011a91c764c46
formegusto <formegusto@gmail.com>
2021-05-20 13:49:51 +0900

    Condition Operator

--- 01-hello-react/src/App.tsx
+++ 01-hello-react/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function App() {
-  const name = '리액트';
+  let name = '리액트';
   return (
     <>
-      <h1>{name} 안녕!</h1>
+      {name === '리액트' && (
+        <h1>리액트입니다.</h1>
+      )} 
       <h2>잘 작동하니?</h2> 
     </>
   );
