e5baf044ec0b4f3723f5e1ca5a7539f233d77768
formegusto <formegusto@gmail.com>
2021-05-21 09:51:30 +0900

    Component Init

---
+++ 02-class-component/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
+}
+
+export default App;
