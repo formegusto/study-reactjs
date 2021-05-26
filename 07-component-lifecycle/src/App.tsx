15f72cc628bc8996f91c748e8c47c1c5a28b933d
formegusto <formegusto@gmail.com>
2021-05-26 17:50:14 +0900

    Component LifeCycle Example

--- 07-component-lifecycle/src/App.tsx
+++ 07-component-lifecycle/src/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import LifeCycleComponent from "./LifeCycleComponent";
 
-function App() {
-  return <LifeCycleComponent />;
+function getRandomColor(): string {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends React.Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleComponent {...this.state} />
+      </>
+    );
+  }
 }
 
 export default App;
--- 07-component-lifecycle/src/LifeCycleComponent.tsx
+++ 07-component-lifecycle/src/LifeCycleComponent.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+type Props = {
+  color: string;
+};
+
 type State = {
   number: number;
   color: string | null;
 };
 
-class LifeCycleComponent extends React.Component<any, State> {
+class LifeCycleComponent extends React.Component<Props, State> {
   state = {
     number: 0,
     color: null,
@@ -13,12 +17,66 @@ class LifeCycleComponent extends React.Component<any, State> {
 
   myRef: React.RefObject<HTMLHeadingElement> = React.createRef();
 
+  constructor(props: Props) {
+    super(props);
+
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
+    console.log("getDerivedStateFromProps", nextProps, prevState);
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
+    console.log("shouldComponentUpdate", nextProps, nextState);
+    return nextState.number % 10 !== 4;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps: Props, prevState: State) {
+    console.log("getSnapshotBeforeUpdate");
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.current!.style.color;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State, snapshot: State) {
+    console.log("componentDidUpdate", prevProps, prevState);
+    if (snapshot) {
+      console.log("업데이트되기 직전 색상: ", snapshot);
+    }
+  }
+
   render() {
+    const style: React.CSSProperties = {
+      color: this.props.color,
+    };
+
     return (
       <div>
-        <h1 ref={this.myRef}>{this.state.number}</h1>
+        <h1 style={style} ref={this.myRef}>
+          {this.state.number}
+        </h1>
         <p>color: {this.state.color}</p>
-        <button>더하기</button>
+        <button onClick={this.handleClick}>더하기</button>
       </div>
     );
   }
