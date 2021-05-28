956a2628b02cf84291168f4782e5338cb57cf6d1
formegusto <formegusto@gmail.com>
2021-05-28 17:48:14 +0900

    CSS Module Styling - classnames

---
+++ 09-component-styling/09-03-css-module-styling/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CSSModuleComponent from "./CSSModuleComponent";
+
+function App() {
+  return (
+    <div>
+      <CSSModuleComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 09-component-styling/09-03-css-module-styling/src/CSSModuleComponent.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles);
+
+function CSSModuleComponent() {
+  console.log(cx("wrapper"));
+  console.log(styles);
+  return (
+    <>
+      <div className={cx("wrapper", "inverted")}>
+        안녕하세요, 저는 <span className="something">CSS Module!</span>
+      </div>
+      <div className={[styles.wrapper, styles.inverted].join(" ")}>
+        안녕하세요, 저는 <span className="something">CSS Module!</span>
+      </div>
+    </>
+  );
+}
+
+export default CSSModuleComponent;
