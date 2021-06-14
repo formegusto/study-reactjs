9221408874ab52e8b35fba72a217e3443702cc5a
formegusto <formegusto@gmail.com>
2021-06-14 10:03:10 +0900

    loadable components

--- 17-code-splitting/src/App.tsx
+++ 17-code-splitting/src/App.tsx
@@ -1,8 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-const SplitMe = React.lazy(() => import("./SplitMe"));
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = React.useState<boolean>(false);
@@ -11,14 +13,18 @@ function App() {
     setVisible(true);
   };
 
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>Loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
