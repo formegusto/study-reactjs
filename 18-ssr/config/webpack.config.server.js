9988b2b8a80e54ade69acb66d7281c2364ab2cdd
formegusto <formegusto@gmail.com>
2021-06-16 09:20:00 +0900

    Static File SSR

--- 18-ssr/config/webpack.config.server.js
+++ 18-ssr/config/webpack.config.server.js
@@ -1,10 +1,16 @@
 const paths = require("./paths");
+const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+const nodeExternals = require("webpack-node-externals");
+const webpack = require("webpack");
+const getClientEnvironment = require("./env");
+
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
-const nodeExternals = require("webpack-node-externals");
+
+const publicUrl = paths.publicUrlOrPath.slice(0, -1);
+const env = getClientEnvironment(publicUrl);
 
 const config = {
   mode: "production", // 프로덕션 모드로 설정하여 최적화 옵션들을 활성화
@@ -20,6 +26,7 @@ const config = {
     rules: [
       {
         oneOf: [
+          // 자바스크립트를 위한 처리
           {
             test: /\.(js|mjs|jsx|ts|tsx)$/,
             include: paths.appSrc,
@@ -47,15 +54,18 @@ const config = {
               compact: false,
             },
           },
+          // CSS 를 위한 처리
           {
             test: cssRegex,
             exclude: cssModuleRegex,
             loader: require.resolve("css-loader"),
           },
+          // CSS Module을 위한 처리
           {
             test: cssModuleRegex,
             loader: require.resolve("css-loader"),
           },
+          // Sass를 위한 처리
           {
             test: sassRegex,
             exclude: sassModuleRegex,
@@ -69,6 +79,7 @@ const config = {
               require.resolve("sass-loader"),
             ],
           },
+          // Sass + CSS Module을 위한 처리
           {
             test: sassRegex,
             exclude: sassModuleRegex,
@@ -84,6 +95,7 @@ const config = {
               require.resolve("sass-loader"),
             ],
           },
+          // url-loader를 위한 설정
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve("url-loader"),
@@ -93,6 +105,8 @@ const config = {
               name: "static/media/[name].[hash:8].[ext]",
             },
           },
+          // 위에서 설정된 확장자를 제외한 파일들은
+          // file-loader를 사용한다.
           {
             loader: require.resolve("file-loader"),
             exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
@@ -109,7 +123,7 @@ const config = {
     modules: ["node_modules"],
     extensions: [".css", ".ts", ".tsx"],
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals(), new webpack.DefinePlugin(env.stringified)], // 환경변수를 주입해준다.],
 };
 
 module.exports = config;
--- 18-ssr/src/index.server.tsx
+++ 18-ssr/src/index.server.tsx
@@ -3,6 +3,41 @@ import ReactDOMServer from "react-dom/server";
 import express from "express";
 import { StaticRouter } from "react-router-dom";
 import SecondApp from "./SecondApp";
+import path from "path";
+import fs from "fs";
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
+);
+
+const chunks = Object.keys(manifest)
+  .filter((key) => /chunk\.js$/.exec(key)) // Chunk로 끝나는 키를 찾아서
+  .map((key) => `<script src=${manifest.files[key]}></script>`) // 스크립트 태그로 변환한다.
+  .join(" ");
+
+function createPage(root: string): string {
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link href="${manifest.files["main.css"]}" rel="stylesheet"/>
+        <title>React App</title>
+      </head>
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root">
+          ${root}
+        </div>
+        <script src="${manifest.files["runtime-main.js"]}"></script>
+          ${chunks}
+        <script src="${manifest.files["main.js"]}"></script>
+      </body>
+    </html>
+    `;
+}
 
 const app = express();
 
@@ -14,9 +49,14 @@ const serverRender = (req: any, res: any, next: any) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx);
-  res.send(root);
+  res.send(createPage(root));
 };
 
+const serve = express.static(path.resolve("./build"), {
+  index: false, // ('/' 경로에서 index.html을 보여주지 않도록 설정)
+});
+
+app.use(serve); // 순서 중요!
 app.use(serverRender);
 
 app.listen(5000, () => {
