e2df482b198adbed93c0f72cf3cf6b14754a7254
formegusto <formegusto@gmail.com>
2021-07-07 10:37:52 +0900

    vanila mobx

--- 19-mobx/19-01-vanila-mobx/index.ts
+++ 19-mobx/19-01-vanila-mobx/index.ts
@@ -1,1 +1,35 @@
+import { action, autorun, makeObservable, observable, reaction } from "mobx";
 
+class Counter {
+  number: number;
+
+  constructor(number: number) {
+    makeObservable(this, {
+      number: observable,
+      increase: action,
+      decrease: action,
+    });
+    document
+      .querySelector(".inc-btn")!
+      .addEventListener("click", this.increase);
+    document
+      .querySelector(".dec-btn")!
+      .addEventListener("click", this.decrease);
+    this.number = number;
+
+    autorun(() => {
+      console.log(this.number);
+      document.querySelector(".counter")!.textContent = this.number.toString();
+    });
+  }
+
+  increase = () => {
+    this.number++;
+  };
+
+  decrease = () => {
+    this.number--;
+  };
+}
+
+new Counter(0);
