a1b372b72e987e837a47b56cf5f1edd33392aea3
formegusto <formegusto@gmail.com>
2021-07-07 12:37:52 +0900

    되아 ㅠㅠㅠ

--- 19-mobx/19-99-decorator/dist/index.js
+++ 19-mobx/19-99-decorator/dist/index.js
@@ -1,12 +1,25 @@
 "use strict";
 // function authCheck(CheckComponent: (...args: any) => void) {
 //   const auth: boolean = false;
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+var __decorate =
+  (this && this.__decorate) ||
+  function (decorators, target, key, desc) {
+    var c = arguments.length,
+      r =
+        c < 3
+          ? target
+          : desc === null
+          ? (desc = Object.getOwnPropertyDescriptor(target, key))
+          : desc,
+      d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function")
+      r = Reflect.decorate(decorators, target, key, desc);
+    else
+      for (var i = decorators.length - 1; i >= 0; i--)
+        if ((d = decorators[i]))
+          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
+  };
 //   return function () {
 //     console.log("Auth Checking...");
 //     CheckComponent(auth);
@@ -18,20 +31,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 // const AuthCheckApp = authCheck(App);
 // AuthCheckApp();
 function authCheck(auth) {
-    return function (CheckComponent) {
-        CheckComponent.prototype.auth = auth;
-    };
+  return function (CheckComponent) {
+    CheckComponent.prototype.auth = auth;
+  };
 }
 var App = /** @class */ (function () {
-    function App() {
-    }
-    App.prototype.render = function () {
-        console.log(this.auth ? "Auth Checking Okay!" : "Need Auth!");
-    };
-    App = __decorate([
-        authCheck(true)
-    ], App);
-    return App;
-}());
+  function App() {}
+  App.prototype.render = function () {
+    console.log(this.auth ? "Auth Checking Okay!" : "Need Auth!");
+  };
+  App = __decorate([authCheck(true)], App);
+  return App;
+})();
 var app = new App();
 app.render();
